94d8fe4fa9d89fa53c986820b3dec215ae5b8d7f
voidilov <voidilov@gmail.com>
2019-08-07 14:38:49 +0300

    Initial Commit

---
+++ SPMAndPod/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SPMAndPod
+//
+//  Created by Ася on 07/08/2019.
+//  Copyright © 2019 voidilov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
